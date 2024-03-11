11deb8c8112a01afa0ee22025ac47af5790625de
jordynbelli <bellijordyn@gmail.com>
2024-03-11 17:56:47 -0500

    starting question 3

---
+++ Python Naming Convention Practice.py
@@ -0,0 +1,10 @@
+# Task 1, Question 3: Code Correction
+Pi_value = 3.14
+userAge = 25
+user_location = "New York"
+MAXLIMIT = 1000
+# Task 2, Question 3: Code Correction Continued
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
