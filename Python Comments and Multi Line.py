4b398054d6eacb2a9e1ffb9dee4d599a0af5ebe8
jordynbelli <bellijordyn@gmail.com>
2024-03-11 03:06:54 -0500

    just in case

--- Python Comments and Multi Line.py
+++
@@ -1,8 +0,0 @@
-#Task 2, Question 1: Your Mood Today
-print("How do you feel today?")
-mood = "happy"
-
-if mood == "happy":
-    print("That's great to hear!")
-if mood == "sad":
-    print("I hope your day gets better!")
