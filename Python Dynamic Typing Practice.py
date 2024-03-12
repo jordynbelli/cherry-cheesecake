e974c835f6a91e28fd639197b7dee7dd7e20f848
jordynbelli <bellijordyn@gmail.com>
2024-03-12 17:36:17 -0500

    python dynamic typing practice

---
+++ Python Dynamic Typing Practice.py
@@ -0,0 +1,9 @@
+# Task 1, Question 4: Code Correction
+string_variable = "This is a string"
+print(string_variable)
+
+number_variable = 100
+print(number_variable)
+
+float_variable = 25.5
+print(float_variable)
