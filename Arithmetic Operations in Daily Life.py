602172a0839dac9bf3ff8cf0e0ef4926429e28be
jordynbelli <bellijordyn@gmail.com>
2024-03-13 01:41:04 -0500

    arithmetic

---
+++ Arithmetic Operations in Daily Life.py
@@ -0,0 +1,12 @@
+# Task 1, Question 5: Grocery Store Math
+apple_price = 1.99
+candy_bar_price = 0.98
+la_croix_price = 4.99
+
+result = 1.99 + 0.98 + 4.99
+print(1.99 + 0.98 + 4.99)
+# Task 2, Question 5: Bank Interest
+bank_balance = 1300
+yearly_interest = 9
+print(bank_balance * yearly_interest)
+
