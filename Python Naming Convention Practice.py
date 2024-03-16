b75eb0f099bcd5b29d2eef6a48e869cf855e7f9a
jordynbelli <bellijordyn@gmail.com>
2024-03-16 16:41:01 -0500

    another save

--- Python Naming Convention Practice.py
+++ Python Naming Convention Practice.py
@@ -1,6 +1,5 @@
-# Task 1, Question 3: Code Correction
-Pi_value = 3.14
-userAge = 25
+# Task 1, Question 2: Code Correction
+PI_VALUE = 3.14
+user_age = 25
 user_location = "New York"
-MAXLIMIT = 1000
-# Task 2, Question 3: Code Correction Continued
+max_limit = 1000
